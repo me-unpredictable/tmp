03a0f622b3de5a5a252e12f91cfecdb9f5422510
meunpredictable <vishalpatel654@gmail.com>
2024-05-17 10:13:35 +1000

    R file without plot

--- 1.r
+++ 1.r
@@ -2,4 +2,7 @@ getwd()
 setwd('/home/meunpredictable/Documents/tmp_git')
 
 data=read.csv('epiSEIHCRD_combAge.csv')
+data$t=as.Date(data$t,origin = '2020-03-01')
+# july to dec
+data_sub=subset(data,subset = (t>='2020-07-01' & t<='2020-12-31'))
 
